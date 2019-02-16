f7f09401bc3937e137d2b0d55557349ccae21e3f
Luiz Felipe <luizfellipsantos@gmail.com>
2019-02-16 21:37:36 -0200

    att one page

--- src/js/app.js
+++
@@ -1,6 +0,0 @@
-function filtrarData() {
-    let dataInicio = document.getElementById('dataInicio')
-    let dataFinal = document.getElementById('dataFinal')
-
-    console.log(dataInicio.value, dataFinal.value)
-};
